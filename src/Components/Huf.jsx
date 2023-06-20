3420b656ec8ab2e9a61b30996d4dcbdd206512cf
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-06-20 16:01:19 +0530

    Added Huf Component

---
+++ src/Components/Huf.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+import {
+  Box,
+  Text,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+} from "@chakra-ui/react";
+
+const Huf = () => {
+  return (
+    <>
+      <Box border={"1px solid #E9E9E9"}>
+        <Flex bg={"#FFFFFF"} w={"95%"} m={"auto"} p={"0.7rem"} pb={"0.1rem"}>
+          <FormControl id="bac">
+            <Flex>
+              <FormLabel fontSize={"1rem"} fontWeight={"normal"}>
+                <Flex>
+                  <Text>Whether opting for taxation under Section</Text>
+                  <Text color={"#3B9AC6"} ml={"0.4rem"}>
+                    115BAC
+                  </Text>
+                  <Text>?</Text>
+                </Flex>
+              </FormLabel>
+              <Select
+                placeholder="Select"
+                name="bac"
+                bg={"#FFFFFF"}
+                w={"10rem"}
+                size={"base"}
+                ml={"31rem"}
+              >
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </Select>
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#FFFFFF"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="taxPayer">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Net Taxable Income
+              </FormLabel>
+
+              <Input
+                name="netTaxableIncome"
+                bg={"#E0E0E0"}
+                w={"16rem"}
+                type="number"
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#F4F4F4"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="incomeTax">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Income Tax
+              </FormLabel>
+
+              <Input
+                name="incomeTax"
+                bg={"#E0E0E0"}
+                w={"16rem"}
+                type="number"
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#FFFFFF"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="surcharge">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Surcharge
+              </FormLabel>
+
+              <Input
+                name="surcharge"
+                bg={"#E0E0E0"}
+                w={"16rem"}
+                type="number"
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#FFFFFF"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="healtheducationcess">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Health and Education cess
+              </FormLabel>
+
+              <Input
+                name="healtheducationcess"
+                bg={"#E0E0E0"}
+                w={"16rem"}
+                type="number"
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#F4F4F4"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="totalTaxLiability">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Total Tax Liability
+              </FormLabel>
+
+              <Input
+                name="totalTaxLiability"
+                bg={"#E0E0E0"}
+                w={"16rem"}
+                type="number"
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#FFFFFF"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="relief">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Relief
+              </FormLabel>
+
+              <Input name="relief" bg={"#F4F4F4"} w={"16rem"} type="number" />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#F4F4F4"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="tcs">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                TDS/TCS/MAT (AMT) Credit Utilized
+              </FormLabel>
+
+              <Input name="tcs" bg={"#FFFFFF"} w={"16rem"} type="number" />
+            </Flex>
+          </FormControl>
+        </Flex>
+
+        <Flex bg={"#FFFFFF"} w={"95%"} m={"auto"} p={"0.7rem"}>
+          <FormControl id="assessedTax">
+            <Flex justifyContent={"space-between"}>
+              <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
+                Assessed Tax
+              </FormLabel>
+
+              <Input
+                name="assessedTax"
+                bg={"#E0E0E0"}
+                w={"16rem"}
+                type="number"
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default Huf;
