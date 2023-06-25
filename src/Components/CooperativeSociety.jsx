e260d55f92ef10c0fe8848923b5037fb51fa9175
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-06-25 22:41:02 +0530

    Added Calculations in Cooperative Society

--- src/Components/CooperativeSociety.jsx
+++ src/Components/CooperativeSociety.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Text,
@@ -8,9 +8,395 @@ import {
   Input,
   Select,
   Button,
+  useToast,
 } from "@chakra-ui/react";
+import AdvanceTaxTable from "./AdvanceTaxTable";
 
 const CooperativeSociety = () => {
+  const [cal, setCal] = useState({
+    netTaxableIncome: "",
+    relief: "",
+    tds: "",
+  });
+
+  const [formData, setFormData] = useState({
+    incomeTax: "",
+    surcharge: "",
+    educationCess: "",
+    secondaryHigherEducationCess: "",
+    totalTaxLiability: "",
+    assessedTax: "",
+  });
+
+  const toast = useToast();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCal((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  // console.log(cal)
+
+  const handleClick = (event) => {
+    const { name, value } = event.target;
+    setCal((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+
+    if (cal.netTaxableIncome <= 250000) {
+      formData.incomeTax = 0;
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = 0;
+      formData.totalTaxLiability = 0;
+      formData.assessedTax = 0;
+
+      setCal((prev) => {
+        return {
+          ...prev,
+          relief: 0,
+          tds: 0,
+        };
+      });
+      toast({
+        position: "top",
+        title: "Income Tax Department",
+        description:
+          "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else if (
+      250001 <= cal.netTaxableIncome &&
+      cal.netTaxableIncome <= 500000
+    ) {
+      formData.incomeTax = (cal.netTaxableIncome * 0.05 - 12500).toFixed(2);
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = (
+        cal.netTaxableIncome / 1000
+      ).toFixed(2);
+      formData.totalTaxLiability = (
+        +formData.secondaryHigherEducationCess + +formData.incomeTax
+      ).toFixed(2);
+      formData.assessedTax = formData.totalTaxLiability - cal.relief;
+      formData.assessedTax = formData.assessedTax - cal.tds;
+      (+formData.assessedTax).toFixed(2);
+
+      if (formData.assessedTax <= 10000) {
+        formData.incomeTax = 0;
+        formData.surcharge = 0;
+        formData.educationCess = 0;
+        formData.secondaryHigherEducationCess = 0;
+        formData.totalTaxLiability = 0;
+        formData.assessedTax = 0;
+        setCal((prev) => {
+          return {
+            ...prev,
+            relief: 0,
+            tds: 0,
+          };
+        });
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description:
+            "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description: `Assessed Tax : ${formData.assessedTax}`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else if (
+      500001 <= cal.netTaxableIncome &&
+      cal.netTaxableIncome <= 750000
+    ) {
+      // console.log(cal.netTaxableIncome);
+      formData.incomeTax = (cal.netTaxableIncome * 0.1 - 12500).toFixed(2);
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = (
+        cal.netTaxableIncome * 0.004
+      ).toFixed(2);
+      formData.totalTaxLiability = (
+        +formData.secondaryHigherEducationCess + +formData.incomeTax
+      ).toFixed(2);
+      formData.assessedTax = formData.totalTaxLiability - cal.relief;
+      formData.assessedTax = formData.assessedTax - cal.tds;
+      (+formData.assessedTax).toFixed(2);
+
+      if (formData.assessedTax <= 10000) {
+        formData.incomeTax = 0;
+        formData.surcharge = 0;
+        formData.educationCess = 0;
+        formData.secondaryHigherEducationCess = 0;
+        formData.totalTaxLiability = 0;
+        formData.assessedTax = 0;
+        setCal((prev) => {
+          return {
+            ...prev,
+            relief: 0,
+            tds: 0,
+          };
+        });
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description:
+            "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description: `Assessed Tax : ${formData.assessedTax}`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else if (
+      750001 <= cal.netTaxableIncome &&
+      cal.netTaxableIncome <= 1000000
+    ) {
+      // console.log(cal.netTaxableIncome);
+      formData.incomeTax = (cal.netTaxableIncome * 0.15 - 37500).toFixed(2);
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = (
+        cal.netTaxableIncome * 0.0045
+      ).toFixed(2);
+      formData.totalTaxLiability = (
+        +formData.secondaryHigherEducationCess + +formData.incomeTax
+      ).toFixed(2);
+      formData.assessedTax = formData.totalTaxLiability - cal.relief;
+      formData.assessedTax = formData.assessedTax - cal.tds;
+      (+formData.assessedTax).toFixed(2);
+
+      if (formData.assessedTax <= 10000) {
+        formData.incomeTax = 0;
+        formData.surcharge = 0;
+        formData.educationCess = 0;
+        formData.secondaryHigherEducationCess = 0;
+        formData.totalTaxLiability = 0;
+        formData.assessedTax = 0;
+        setCal((prev) => {
+          return {
+            ...prev,
+            relief: 0,
+            tds: 0,
+          };
+        });
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description:
+            "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description: `Assessed Tax : ${formData.assessedTax}`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else if (
+      1000001 <= cal.netTaxableIncome &&
+      cal.netTaxableIncome <= 1250000
+    ) {
+      // console.log(cal.netTaxableIncome);
+      formData.incomeTax = (cal.netTaxableIncome * 0.2 - 62500).toFixed(2);
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = (
+        cal.netTaxableIncome * 0.006
+      ).toFixed(2);
+      formData.totalTaxLiability = (
+        +formData.secondaryHigherEducationCess + +formData.incomeTax
+      ).toFixed(2);
+      formData.assessedTax = formData.totalTaxLiability - cal.relief;
+      formData.assessedTax = formData.assessedTax - cal.tds;
+      (+formData.assessedTax).toFixed(2);
+
+      if (formData.assessedTax <= 10000) {
+        formData.incomeTax = 0;
+        formData.surcharge = 0;
+        formData.educationCess = 0;
+        formData.secondaryHigherEducationCess = 0;
+        formData.totalTaxLiability = 0;
+        formData.assessedTax = 0;
+        setCal((prev) => {
+          return {
+            ...prev,
+            relief: 0,
+            tds: 0,
+          };
+        });
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description:
+            "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description: `Assessed Tax : ${formData.assessedTax}`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else if (
+      1250001 <= cal.netTaxableIncome &&
+      cal.netTaxableIncome <= 1500000
+    ) {
+      // console.log(cal.netTaxableIncome);
+      formData.incomeTax = (cal.netTaxableIncome * 0.25 - 112500).toFixed(2);
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = (
+        cal.netTaxableIncome * 0.007
+      ).toFixed(2);
+      formData.totalTaxLiability = (
+        +formData.secondaryHigherEducationCess + +formData.incomeTax
+      ).toFixed(2);
+      formData.assessedTax = formData.totalTaxLiability - cal.relief;
+      formData.assessedTax = formData.assessedTax - cal.tds;
+      (+formData.assessedTax).toFixed(2);
+
+      if (formData.assessedTax <= 10000) {
+        formData.incomeTax = 0;
+        formData.surcharge = 0;
+        formData.educationCess = 0;
+        formData.secondaryHigherEducationCess = 0;
+        formData.totalTaxLiability = 0;
+        formData.assessedTax = 0;
+        setCal((prev) => {
+          return {
+            ...prev,
+            relief: 0,
+            tds: 0,
+          };
+        });
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description:
+            "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description: `Assessed Tax : ${formData.assessedTax}`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else if (cal.netTaxableIncome > 1500000) {
+      // console.log(cal.netTaxableIncome);
+      formData.incomeTax = (cal.netTaxableIncome * 0.3 - 188000).toFixed(2);
+      formData.surcharge = 0;
+      formData.educationCess = 0;
+      formData.secondaryHigherEducationCess = (
+        cal.netTaxableIncome * 0.008
+      ).toFixed(2);
+      formData.totalTaxLiability = (
+        +formData.secondaryHigherEducationCess + +formData.incomeTax
+      ).toFixed(2);
+      formData.assessedTax = formData.totalTaxLiability - cal.relief;
+      formData.assessedTax = formData.assessedTax - cal.tds;
+      (+formData.assessedTax).toFixed(2);
+
+      if (formData.assessedTax <= 10000) {
+        formData.incomeTax = 0;
+        formData.surcharge = 0;
+        formData.educationCess = 0;
+        formData.secondaryHigherEducationCess = 0;
+        formData.totalTaxLiability = 0;
+        formData.assessedTax = 0;
+        setCal((prev) => {
+          return {
+            ...prev,
+            relief: 0,
+            tds: 0,
+          };
+        });
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description:
+            "Error:Advance Tax will not be payable if assessed tax is less than ₹10,000",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "Income Tax Department",
+          description: `Assessed Tax : ${formData.assessedTax}`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    }
+  };
+
+  const handleReset = () => {
+    setCal({
+      netTaxableIncome: "",
+      relief: "",
+      tds: "",
+    });
+
+    setFormData({
+      incomeTax: "",
+      surcharge: "",
+      educationCess: "",
+      secondaryHigherEducationCess: "",
+      totalTaxLiability: "",
+      assessedTax: "",
+    });
+  };
+
   return (
     <>
       <Box border={"1px solid #E9E9E9"}>
@@ -81,6 +467,8 @@ const CooperativeSociety = () => {
 
               <Input
                 name="netTaxableIncome"
+                value={cal.netTaxableIncome}
+                onChange={handleChange}
                 bg={"#E0E0E0"}
                 w={"16rem"}
                 type="number"
@@ -98,9 +486,12 @@ const CooperativeSociety = () => {
 
               <Input
                 name="incomeTax"
+                value={formData.incomeTax}
                 bg={"#E0E0E0"}
                 w={"16rem"}
                 type="number"
+                disabled
+                fontWeight={"bold"}
               />
             </Flex>
           </FormControl>
@@ -115,26 +506,31 @@ const CooperativeSociety = () => {
 
               <Input
                 name="surcharge"
+                value={formData.surcharge}
                 bg={"#E0E0E0"}
                 w={"16rem"}
                 type="number"
+                disabled
               />
             </Flex>
           </FormControl>
         </Flex>
 
         <Flex bg={"#FFFFFF"} w={"100%"} m={"auto"} p={"0.7rem"}>
-          <FormControl id="healtheducationcess">
+          <FormControl id="secondaryHigherEducationCess">
             <Flex justifyContent={"space-between"}>
               <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
                 Health and Education cess
               </FormLabel>
 
               <Input
-                name="healtheducationcess"
+                name="secondaryHigherEducationCess"
+                value={formData.secondaryHigherEducationCess}
                 bg={"#E0E0E0"}
                 w={"16rem"}
                 type="number"
+                disabled
+                fontWeight={"bold"}
               />
             </Flex>
           </FormControl>
@@ -149,9 +545,12 @@ const CooperativeSociety = () => {
 
               <Input
                 name="totalTaxLiability"
+                value={formData.totalTaxLiability}
                 bg={"#E0E0E0"}
                 w={"16rem"}
                 type="number"
+                disabled
+                fontWeight={"bold"}
               />
             </Flex>
           </FormControl>
@@ -164,19 +563,35 @@ const CooperativeSociety = () => {
                 Relief
               </FormLabel>
 
-              <Input name="relief" bg={"#F4F4F4"} w={"16rem"} type="number" />
+              <Input
+                name="relief"
+                value={cal.relief}
+                onChange={handleChange}
+                bg={"#F4F4F4"}
+                w={"16rem"}
+                type="number"
+                fontWeight={"bold"}
+              />
             </Flex>
           </FormControl>
         </Flex>
 
         <Flex bg={"#F4F4F4"} w={"100%"} m={"auto"} p={"0.7rem"}>
-          <FormControl id="tcs">
+          <FormControl id="tds">
             <Flex justifyContent={"space-between"}>
               <FormLabel fontSize={"1.2rem"} fontWeight={"normal"}>
                 TDS/TCS/MAT (AMT) Credit Utilized
               </FormLabel>
 
-              <Input name="tcs" bg={"#FFFFFF"} w={"16rem"} type="number" />
+              <Input
+                name="tds"
+                value={cal.tds}
+                onChange={handleChange}
+                bg={"#FFFFFF"}
+                w={"16rem"}
+                type="number"
+                fontWeight={"bold"}
+              />
             </Flex>
           </FormControl>
         </Flex>
@@ -190,9 +605,12 @@ const CooperativeSociety = () => {
 
               <Input
                 name="assessedTax"
+                value={formData.assessedTax}
                 bg={"#E0E0E0"}
                 w={"16rem"}
                 type="number"
+                disabled
+                fontWeight={"bold"}
               />
             </Flex>
           </FormControl>
@@ -200,32 +618,39 @@ const CooperativeSociety = () => {
       </Box>
 
       <Box w={"20rem"} m={"auto"} mt={"0.7rem"} mb={"1rem"}>
-            <Button
-              color={"#FFFFFF"}
-              bg={"#FE6C5F"}
-              fontSize={"0.9rem"}
-              borderRadius={"0rem"}
-              _hover={{
-                color: "#FFFFFF",
-                bg: "#FE6C5F",
-              }}
-            >
-              Calculate
-            </Button>
-            <Button
-              color={"#FFFFFF"}
-              bg={"#A0A1A1"}
-              ml={"1rem"}
-              borderRadius={"0rem"}
-              _hover={{
-                color: "#FFFFFF",
-                bg: "#A0A1A1",
-              }}
-            >
-              Reset
-            </Button>
-          </Box>
+        <Button
+          color={"#FFFFFF"}
+          bg={"#FE6C5F"}
+          fontSize={"0.9rem"}
+          borderRadius={"0rem"}
+          _hover={{
+            color: "#FFFFFF",
+            bg: "#FE6C5F",
+          }}
+          onClick={handleClick}
+        >
+          Calculate
+        </Button>
+        <Button
+          color={"#FFFFFF"}
+          bg={"#A0A1A1"}
+          ml={"1rem"}
+          borderRadius={"0rem"}
+          _hover={{
+            color: "#FFFFFF",
+            bg: "#A0A1A1",
+          }}
+          onClick={handleReset}
+        >
+          Reset
+        </Button>
+      </Box>
 
+      {+formData.assessedTax > 0 ? (
+        <AdvanceTaxTable TableData={formData.assessedTax} />
+      ) : (
+        ""
+      )}
     </>
   );
 };
